58dadbba8d5d6988e4afe78b165a9d49890a3880
Xu He <he275@purdue.edu>
2019-12-06 02:34:07 -0500

    finish modal UI

--- js/main.js
+++ js/main.js
@@ -2,13 +2,22 @@ var X = 'X';
 var O = 'O';
 
 function generateModal(id) {
-  var modal = document.createElement('div');
-	modal.className = 'modal';
-  if (id) modal.id = id;
-  var content = document.createElement('div');
-  content.className = 'modal-content';
+  var container = document.getElementById('body-container');  
+  var modal = generateDiv('modal', id);
+  var content = generateDiv('modal-content');
   modal.appendChild(content);
-  return modal;
+  container.appendChild(modal);
+  return content;
+}
+
+function showModal() {
+  var modal = document.getElementsByClassName('modal');
+  if (modal) modal[0].classList.add('show-modal');
+}
+
+function dismissModal() {
+  var modal = document.getElementsByClassName('modal');
+  if (modal) modal.classList.remove('show-modal');
 }
 
 function generateButton(className, id, text, callback) {
@@ -27,24 +36,45 @@ function generateButton(className, id, text, callback) {
   return button;
 }
 
+function generateDiv(className, id, text, callback) {
+  var div = document.createElement('div');
+  div.className = 'div';
+  if (className) div.classList.add(className);
+  if (id) div.id = id;
+  if (text) {
+    div.innerHTML = text;
+  }
+  if(callback)  div.onclick = callback;
+  
+  return div;
+}
+
 function init() {
-	var modal = generateModal();
-  modal.innerHTML = 'Choose a symbol: ';
+  var body = document.getElementById('body-container');
+
+  var modal = generateModal();
+  var label = generateDiv('label', 'symbol-option', 'Choose a symbol: ');
+  modal.appendChild(label);
 
   var xCallback = function() {
 		localStorage.setItem('compToken', X);	
     console.log('player chose x.');
 	};
-	var xOption = generateButton('optionButton', 'label-x', 'X', xCallback);
+	var xOption = generateButton('optionButton', 'option-x', 'X', xCallback);
 	
   var oCallback = function() {
 		localStorage.setItem('compToken', O);
     console.log('player chose o.');
 	};
-	var oOption = generateButton('optionButton', 'label-o', 'O', oCallback);
+	var oOption = generateButton('optionButton', 'option-o', 'O', oCallback);
 
   modal.appendChild(xOption);
   modal.appendChild(oOption);
+
+  //generate board in the background
+  var container = generateDiv('container', 'main-container');
+  body.appendChild(container);
+  showModal();  
 }
 
 init();
